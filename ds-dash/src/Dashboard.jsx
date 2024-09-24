bde630173e49597c09a828cfe818e704f50f6344
Rahul S <rahul.suresh307@gmail.com>
2024-09-24 18:34:54 +0530

    intial commit

---
+++ ds-dash/src/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Badge, Button, Notification, InputBox, Dropdown } from 'rahul-ds';
+import '../node_modules/rahul-ds/src/style.css'
+
+const Dashboard = () => {
+  return (
+     <div>
+      <h1>Dashboard</h1>
+    </div>
+
+  )
+}
+
+export default Dashboard
